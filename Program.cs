e4acbca89e915ed8586086be3b86f62c1b875a74
Sarah <Sarah@HP-SARAHI>
2023-09-20 14:58:13 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a linked list
+            LinkedList<int> linkedList = new LinkedList<int>();
+
+            // Add elements to the linked list
+            linkedList.AddLast(1);
+            linkedList.AddLast(2);
+            linkedList.AddLast(3);
+
+            // Iterate through the list and print the elements
+            foreach (int element in linkedList)
+            {
+                Console.WriteLine("Element in the list: " + element);
+            }
+        }
+    }
+}
